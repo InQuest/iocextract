b11cf257fd9aaca89011eaf33ac8045a76e39d76
rshipp <github@rshipp.com>
2018-09-20 16:00:33 -0500

    Bump version to v1.9.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='iocextract',
-    version='1.8.0',
+    version='1.9.0',
     include_package_data=True,
     py_modules=[
         'iocextract',
