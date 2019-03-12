a2aa9ccdfc7eafb56be265bbd2e1173807291a19
rshipp <github@rshipp.com>
2019-03-12 11:08:42 -0500

    Bump version to v1.13.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='iocextract',
-    version='1.13.0',
+    version='1.13.1',
     include_package_data=True,
     py_modules=[
         'iocextract',
