62827f3dafcfa094b5a5304dd53387cd746560d7
azazelm3dj3d <56496067+azazelm3dj3d@users.noreply.github.com>
2023-03-15 08:57:34 -0500

    Hot fix for the README on PyPI

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 import os
 from setuptools import setup
 
-# README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+with open("README.md", "r", encoding = "utf-8") as fh:
+    README = fh.read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='iocextract',
-    version='1.15.0',
+    version='1.15.1',
     include_package_data=True,
     py_modules=['iocextract',],
     install_requires=['regex',],
@@ -24,7 +25,8 @@ setup(
     },
     license='GPL',
     description='Advanced Indicator of Compromise (IOC) extractor.',
-    long_description="README.md",
+    long_description=README,
+    long_description_content_type = "text/markdown",
     url='https://github.com/InQuest/python-iocextract',
     author='InQuest Labs',
     author_email='labs@inquest.net',
